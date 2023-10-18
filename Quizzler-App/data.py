40e3c9f20c591f553e82119829283f3518a610ce
Finneas Sensiba <142452994+astralm0nke@users.noreply.github.com>
2023-10-18 19:10:29 -0400

    Delete Quizzler-App directory

--- Quizzler-App/data.py
+++
@@ -1,7 +0,0 @@
-import requests
-params = {'amount': 10, 'type': 'boolean'}
-response = requests.get(url='https://opentdb.com/api.php', params=params)
-response.raise_for_status()
-data = response.json()
-question_data = data['results']
-print(question_data)
