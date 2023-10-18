4f04d196342dd2efc15394a7137c722b67a4764e
Finneas Sensiba <142452994+astralm0nke@users.noreply.github.com>
2023-10-18 17:11:11 -0400

    Add files via upload

---
+++ THUS-SPAKE-YE/Ye_quotes_app.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import requests
+
+def get_quote():
+    response = requests.get('https://api.kanye.rest')
+    response.raise_for_status()
+    data = response.json()
+    ye_quote = data['quote']
+    canvas.itemconfig(quote_text, text=ye_quote)
+
+
+window = Tk()
+window.title("THUS SPAKE YE...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="/Users/finneassensiba/pystuff/100_days/kanye-quotes-app/background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="What will Ye say???", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="/Users/finneassensiba/pystuff/100_days/kanye-quotes-app/kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+
+window.mainloop()
