e49747874f8b0bd5d1a0995504d66ef7ccda763a
Finneas Sensiba <142452994+astralm0nke@users.noreply.github.com>
2024-02-23 14:28:01 -0500

    Update stock_phone_notifs.py

--- stock_phone_notifs.py
+++ stock_phone_notifs.py
@@ -6,7 +6,7 @@ COMPANY_NAME = {NAME-> str}
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
-NEWS_KEY = os.environ.get('TWILIO_NEWS_KEY')
+NEWS_KEY = os.environ.get('NEWS_API_KEY')
 TWILIO_SID = os.environ.get("TWILIO_SID')
 TWILIO_AUTH = os.environ.get("TWILIO_AUTH_TOKEN')
 
